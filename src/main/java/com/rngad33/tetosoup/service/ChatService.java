c0aadb29ca246392aac8d945e87736a1fd6cc93f
RngAd33 <yxbsyx@outlook.com>
2025-04-10 10:17:09 +0800

    changing

--- src/main/java/com/rngad33/tetosoup/service/ChatService.java
+++ src/main/java/com/rngad33/tetosoup/service/ChatService.java
@@ -4,4 +4,13 @@ package com.rngad33.tetosoup.service;
  * 客户端服务接口
  */
 public interface ChatService {
+
+    /**
+     * 与 AI 对话
+     *
+     * @param message 用户输入的信息
+     * @return 返回结果
+     */
+    String doChat(String message);
+
 }
